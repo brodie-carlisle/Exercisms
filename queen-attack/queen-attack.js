4947d524a43d298f7ececb2a131e90f597a7a297
Brodie <brodiecarlisle@gmail.com>
2019-10-02 23:59:15 -0600

    added queen attact, modified scrabble

--- queen-attack/queen-attack.js
+++ queen-attack/queen-attack.js
@@ -1,5 +1,5 @@
 export class QueenAttack {
-  constructor(position = {}) {
+  constructor(position = {}) { //constructor accepts an obj param
     this.white = position.white || [0, 3]; //accepts params or sets default position
     this.black = position.black || [7, 3];
     this.board = this.createBoard();
@@ -20,28 +20,28 @@ export class QueenAttack {
       .map(() => Array(8).fill("_"));
   }
   setQueens() {
-    //places queens on 'board'
-    const [x1, y1] = this.white;
+    //calls createBoard() via this.board then places queens on 'board'
+    const [x1, y1] = this.white; //assigns values to x & y by calling constructor
     const [x2, y2] = this.black;
 
-    this.board[x1][y1] = "W";
+    this.board[x1][y1] = "W"; //uses assigned values to place pieces on board
     this.board[x2][y2] = "B";
   }
-  toString() {
+  toString() { //toString() method is called in 2 tests
     this.setQueens();
     let boardStr = "";
 
     for (let i = 0; i < this.board.length; i++) {
-      // loop through arrays and transforms from an array of 8 nested arrays, to a string with 8 lines.
+      // loop through arrays and transforms from an array of 8 nested arrays, to an array with 8 strings (lines).
       boardStr += this.board[i].join(" ") + "\n";
     }
     return boardStr;
   }
   canAttack() {
     let attack = false
-    if (this.white[0] === this.black[0] || this.white[1] === this.black[1]) {//checks rows and columns
+    if (this.white[0] === this.black[0] || this.white[1] === this.black[1]) {//checks rows and columns, if the x axis (index[0]) or y axis (index[1]) are the same they share a row or column
       return attack = true;
-    } else if (//checks diagonals 
+    } else if (//checks diagonals. If diagonally alligned, the sum or difference in cooridnates will be the same
       this.white[0] - this.white[1] === this.black[0] - this.black[1] ||
       this.white[0] + this.white[1] === this.black[0] + this.black[1]
     ) {
